9c75432109b8bcd2a8d78f1a7cd425d6f8f9412b
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-04-13 11:10:04 -0500

    updates .gitignore and parks_migration.php

--- parks_migration.php
+++ parks_migration.php
@@ -4,16 +4,18 @@ require __DIR__ . '/testdb.php';
 
 
 $query = 'DROP TABLE IF EXISTS national_parks;';
+$dbc->exec($query);
 
-$natParks = 'CREATE TABLE national_parks (
-id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-name VARCHAR (100) NOT NULL,
-location VARCHAR(100) NOT NULL, 
-date_established DATE,
-area_in_acres INT,
-PRIMARY KEY (id)
-);';
-
+$query = <<<SQL
+CREATE TABLE national_parks (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	name VARCHAR (100) NOT NULL,
+	location VARCHAR(100) NOT NULL, 
+	date_established DATE,
+	area_in_acres INT,
+	description TEXT,
+	PRIMARY KEY (id)
+);
+SQL;
 
 $dbc->exec($query);
-$dbc->exec($natParks);
